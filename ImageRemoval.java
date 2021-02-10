edd95259a8445e556b74062c7b96c1cbaadbcb07
yuriydev214 <69816976+yuriydev214@users.noreply.github.com>
2021-02-10 20:05:20 +0300

    Create ImageRemoval.java

---
+++ ImageRemoval.java
@@ -0,0 +1,5 @@
+Can we chat via Skype for better communication?
+My skype address is jnx_team@outlook.com.
+Please add me as contact.
+Please don't mention about Skype on freelancer chat session. it is the violation of this site's rule.
+Thanks.
